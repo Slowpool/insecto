6a816fe7e65800d2fa8825a45b62a161805439f9
slowpool <nevznachay02@gmail.com>
2025-01-30 21:52:07 +0300

    unclosed tag was a culprit of strange behavior. now <head> is rendered appropriately

--- app-php/views/search/index.php
+++ app-php/views/search/index.php
@@ -1,16 +1,16 @@
 <?php
 
 /** @var yii\web\View $this */
-/** @var yii\widgets\ActiveForm $searchForm */
+/** @var yii\bootstrap5\ActiveForm $searchForm */
 /** @var app\models\search\SearchPageModel $searchPageModel */
 
 use yii\helpers\Url;
-use yii\widgets\ActiveForm;
+use yii\bootstrap5\ActiveForm;
 use app\models\search\SearchModel;
 
 
 $this->title = 'Search';
-$this->registerLinkTag(['rel' => 'canonical', 'href' => Url::to('/insects', true)]); 
+$this->registerLinkTag(['rel' => 'canonical', 'href' => Url::to('/insects', true)]);
 
 $searchModel = $searchPageModel->searchModel;
 
@@ -18,19 +18,15 @@ $categoryNames = array_keys($searchModel->categories);
 $categoryItems = array_combine($categoryNames, $categoryNames);
 
 // simplifying access to category name. just to simplify the code in further
-foreach($searchPageModel->cardsWithGoods as &$card) {
+foreach ($searchPageModel->cardsWithGoods as &$card) {
     $card['category'] = $card['category']['name'];
 }
 unset($card);
 
-
 ?>
 
-div id = search-page is below
 <div id="search-page">
-    search tag is below
     <search>
-        search tag is above
         <?php $searchForm = ActiveForm::begin() ?>
         <?= $searchForm->field($searchModel, 'searchText')->textInput(['placeholder' => 'Search...']) ?>
         <section class="search-categories">
@@ -42,5 +38,6 @@ div id = search-page is below
             <?= $searchForm->field($searchModel, 'minPrice')->textInput(['type' => 'number', 'placeholder' => '0', 'min' => '0', 'max' => strval(PHP_INT_MAX)]) ?>
             <?= $searchForm->field($searchModel, 'maxPrice')->textInput(['type' => 'number', 'placeholder' => strval(PHP_INT_MAX), 'min' => '0', 'max' => strval(PHP_INT_MAX)]) ?>
         </section>
+        <?php ActiveForm::end() ?>
     </search>
 </div>
